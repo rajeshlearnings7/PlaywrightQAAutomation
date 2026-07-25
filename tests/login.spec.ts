6affd7057a5ec8bd673b2e407b6fa6527ed1d843
rajeshlearnings7 <rajeshlearnings7@gmail.com>
2026-07-26 00:24:14 +0530

    Updated the file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,17 @@
 import {test, expect} from "@playwright/test";
 
+/*
+
+*/
 test("Launch the browser", async ({page})=>{
     await page.goto("https://sgtestinginstituteapp.onrender.com/");
+    // Storing the page title here
+    const title = await page.title();
+    const url = await page.url();
+    console.info("The url is "+url);
+    console.info("The title is "+title);
     await page.locator("//input[@name='username']").fill("rajeshlearnings7");
     await page.locator("//input[@name='password']").fill("Rajeshlearnings7@");
     await page.getByRole('button', { name: 'Sign In' }).click();
-    console.info("First test successful");
+    console.info("First test successful on login page");
 })
