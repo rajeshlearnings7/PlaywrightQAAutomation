52820dbd79ff7e23754d5cd527dc4c7497b7c6d5
rajeshlearnings7 <rajeshlearnings7@gmail.com>
2026-07-26 00:48:30 +0530

    Added the fwaits

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,13 +5,17 @@ import {test, expect} from "@playwright/test";
 */
 test("Launch the browser", async ({page})=>{
     await page.goto("https://sgtestinginstituteapp.onrender.com/");
+    await page.waitForTimeout(6000);
     // Storing the page title here
     const title = await page.title();
     const url = await page.url();
     console.info("The url is "+url);
     console.info("The title is "+title);
     await page.locator("//input[@name='username']").fill("rajeshlearnings7");
+    await page.waitForTimeout(5000);
     await page.locator("//input[@name='password']").fill("Rajeshlearnings7@");
+    await page.waitForTimeout(5000);
     await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.waitForTimeout(5000);
     console.info("First test successful on login page");
 })
